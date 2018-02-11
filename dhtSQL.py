ee0b830518da71b0403e6c6ca1244bf4f8a691fc
JoanRr <joarru12@htl-shkoder.com>
2018-02-11 17:56:14 +0100

    Update dhtSQL.py

--- dhtSQL.py
+++ dhtSQL.py
@@ -9,11 +9,13 @@ DHTPIN = 17
 result = read_dht11_dat()
 temperature, humidity = result
 
+# Ueberpruefung der Sensor-Skript
 if humidity is not None and temperature is not None:
     print 'Temperature={0:0.1f} Humidity={1:0.1f}'.format(humidity, temperature)
 else:
     print 'Failed to get a reading. Please try again!'
 
+# Verbindung mit der Datenbank erstellen
 db = MySQLdb.connect("localhost", "root", "smartvehicle", "temp_database")
 c = db.cursor()
 
@@ -22,16 +24,17 @@ print temperature
 temperature1 = temperature
 print humidity
 
+# Die Werte wurden auf der Datenbank hinzugefuegt.
 sql= ("INSERT INTO tempLog (temperature, humidity) VALUES (%s,%s)", (humidity1,temperature1))
 try:
-   print "Writing to database..."
+   print "Writing to database..."       # Ueberpruefung ob der Datenuebertragung erfolgreich realisiert wurde oder nicht.
    c.execute(*sql)
    db.commit()
-   print "Write Complete"
+   print "Write Complete"               # Wenn ja, dann wurde "Write Complete" angezeigt.
 except:
     b.rollback()
-    print "failed"
+    print "failed"                      # Wenn nicht, dann wurde "failed" angezeigt.
 
-c.close()
+c.close()                               # Verbindung schliessen.
 db.close()
 
